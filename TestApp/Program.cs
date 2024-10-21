25f1344dd48bce599ec8339f8e8d54d8db78a34f
MandicM11 <mirko.mandic996@gmail.com>
2024-10-21 13:19:22 +0200

    init

---
+++ TestApp/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+using MQTTnet;
+using MQTTnet.Client;
+using MQTTnet.Protocol;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        string broker = "test.mosquitto.org";
+        int port = 1883;
+        string clientId = Guid.NewGuid().ToString();
+        string topic = "Test/mqtt";
+
+        // Create a MQTT client factory
+        var factory = new MqttFactory();
+
+        // Create a MQTT client instance
+        var mqttClient = factory.CreateMqttClient();
+
+        // Create MQTT client options
+        var options = new MqttClientOptionsBuilder()
+            .WithTcpServer(broker, port) // MQTT broker address and port
+            .WithClientId(clientId)
+            .Build();
+
+        // Connect to MQTT broker
+        var connectResult = await mqttClient.ConnectAsync(options);
+
+        if (connectResult.ResultCode == MqttClientConnectResultCode.Success)
+        {
+            Console.WriteLine("Connected to MQTT broker successfully.");
+
+            // Subscribe to a topic
+            await mqttClient.SubscribeAsync(topic);
+
+            // Callback function when a message is received
+            mqttClient.ApplicationMessageReceivedAsync += e =>
+            {
+                Console.WriteLine($"Received message: {Encoding.UTF8.GetString(e.ApplicationMessage.PayloadSegment)}");
+                return Task.CompletedTask;
+            };
+
+            // Sends messages until you brake the loop by not saying y
+            bool sendMessage = true;
+            while (sendMessage)
+            {
+                {
+                    var message = new MqttApplicationMessageBuilder()
+                        .WithTopic(topic)
+                        .WithPayload($"Hello, World!")
+                        .WithQualityOfServiceLevel(MqttQualityOfServiceLevel.AtLeastOnce)
+                        .WithRetainFlag()
+                        .Build();
+
+                    await mqttClient.PublishAsync(message);
+                    Console.WriteLine("\nSend another alert? (y/n)");
+                    sendMessage = Console.ReadLine()!.ToLower() == "y";
+
+                }
+            }
+
+                // Unsubscribe and disconnect
+                await mqttClient.UnsubscribeAsync(topic);
+            await mqttClient.DisconnectAsync();
+        }
+        else
+        {
+            Console.WriteLine($"Failed to connect to MQTT broker: {connectResult.ResultCode}");
+        }
+    }
+}
